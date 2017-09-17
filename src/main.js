5123132d4b6f86ce3c3f081fa302577916045166
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-09-18 01:48:44 +0300

    Çoklu Dil eklendi.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,49 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+/*
+import jQuery from 'jquery'
+global.jQuery = jQuery
+import tether from 'tether'
+global.Tether = tether;
+import 'bootstrap/dist/css/bootstrap.css'
+
+let Bootstrap = require('bootstrap');
+*/
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    "headerLink": {
+      "home": "Home",
+      "about": "About",
+      "contact": "Contact"
+    },
+    "locale":{
+      "tr": "Turkish",
+      "en": "English"
+    }
+  },
+  tr: {
+    "headerLink": {
+      "home": "Anasayfa",
+      "about": "Hakkımızda",
+      "contact": "İletişim"
+    },
+    "locale":{
+      "tr": "Türkçe",
+      "en": "İngilizce"
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'tr',
+  messages
+})
 
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App)
 })
